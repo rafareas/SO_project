415fd426fe6c81f02192dcd6e2fa1dbc3ae5ffa1
joodanic <a94015@alunos.uminho.pt>
2023-04-17 22:03:00 +0100

    organizei o codigo

--- tracer.c
+++ tracer.c
@@ -7,32 +7,115 @@
 #include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
 #include <sys/wait.h> /* chamadas wait*() e macros relacionadas */
 #include <time.h>
+#include <string.h>
 
-int executa(char** comando){
+int executaU(char* comando){        
     int return_exec;
     int status;
+    int k = 0;
+    int i=0;
     
-    //printf("%s\n",comando[0]);
+    char *exec_args[20];
+    char *nova_string=strdup(comando);
+    char *string;
+
+    while((string=strsep(&nova_string," "))!=NULL){
+        //printf("%s\n\n",string);
+        exec_args[i]=string;
+        //printf("%s\n",exec_args[i]);
+        i++;
+    }
+
+    exec_args[i]=NULL;
 
     pid_t res = fork();
+    
+    if(res<0){
+        perror("Erro no fork");
+    }
+    if(res==0){
+        return_exec=execvp(exec_args[0],exec_args);
+        _exit(return_exec);
+        }
 
-        if(res<0){
-            perror("Error using fork");
-        }
-        else if(res==0){
-            //codigo do filho
-            //printf("FILHO\n");
-           
-           return_exec=execvp(comando[0],comando);
-           
-           _exit(return_exec);
-        }
-        else{
+    else{
             pid_t wait_pid = wait(&status);
             if(WIFEXITED(status)){
                 printf("Pai o filho %d terminou com exit code %d\n",wait_pid,WEXITSTATUS(status));
             }
+        }    
+}
+
+//funciona com apenas dois programas
+int executaP(char* comando){        
+    int return_exec;
+    int status;
+    int k=0;
+    int i=0;
+    
+    int array_pid[20];
+    char *exec_args[20];
+    char *nova_string=strdup(comando);
+    char *string;
+
+    int fds[2];
+    int return_pipe = pipe(fds);
+
+    while((string=strsep(&nova_string,"|"))!=NULL){
+        //printf("%s\n\n",string);
+        exec_args[i]=string;
+        //printf("%s\n",exec_args[i]);
+        i++;   
+    }
+
+    for(k=0;k<i;k++){
+        pid_t res = fork();
+        int j=0;
+        char *string2;
+        char *string3;
+        char *exec_args2[20];
+
+        
+        if(res==0){
+        string2=exec_args[k];
+        //printf("%s\n",string2);
+        string3 = strtok(string2," ");
+        while(string3!=NULL){
+            exec_args2[j]=string3;
+            //printf("%s\n",exec_args2[j]);
+            string3=strtok(NULL," ");
+            j++;
         }
+        exec_args2[j]=NULL;
+
+        if(k==0){
+            dup2(fds[1],STDOUT_FILENO);
+            close(fds[0]);
+            close(fds[1]);
+            return_exec=execvp(exec_args2[0],exec_args2);
+            _exit(return_exec);
+        }
+        else{
+            dup2(fds[0],STDIN_FILENO);
+            close(fds[0]);
+            close(fds[1]);
+            return_exec=execvp(exec_args2[0],exec_args2);
+            _exit(return_exec);
+        }
+        }
+        else{
+            array_pid[k] = res;
+        }
+    }
+
+    for (int l=0;l<i;l++){
+            pid_t wait_pid = waitpid(array_pid[l],&status,0);
+            if(WIFEXITED(status)){
+                printf("Pai o filho %d terminou com exit code %d\n",wait_pid,WEXITSTATUS(status));
+            }
+            close(fds[0]);
+            close(fds[1]);
+        }    
 }
 
 
@@ -59,18 +142,23 @@ int main(int argc, char **argv){
         if(strcmp(argv[2],"-u")==0){
             int i=3;
             int k=0;
-            char *comando[argc];
-            while(i<argc && k<argc){
-                comando[k] = strdup(argv[i]);
-                //printf("%s\n",comando[k]);
-                k++;
-                i++;
-            }
-            comando[k]=NULL;
-            //printf("%s\n\n",comando[0]);
+            char *comando = strdup(argv[3]);
+            //printf("%s\n\n",comando);
             
             start_t = clock();
-            ret = executa(comando);
+            ret = executaU(comando);
+            end_t = clock();
+            total_t = (double) (end_t-start_t) / CLOCKS_PER_SEC;
+            printf("%f\n",total_t);
+        }
+        else if(strcmp(argv[2],"-p")==0){
+            int i=3;
+            int k=0;
+            char *comando = strdup(argv[3]);
+            //printf("%s\n\n",comando);
+            
+            start_t = clock();
+            ret = executaP(comando);
             end_t = clock();
             total_t = (double) (end_t-start_t) / CLOCKS_PER_SEC;
             printf("%f\n",total_t);
@@ -80,6 +168,7 @@ int main(int argc, char **argv){
     write(fd,argv[1],sizeof(argv[1]));
 
     close(fd);
+    unlink("fifo");
     printf("Terminei\n");
 
     return 0;
