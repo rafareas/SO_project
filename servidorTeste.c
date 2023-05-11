e2a30d929cb4e7701cc79e8152ff8a860ceb558d
Rafael dos Anjos Arêas <40003878+rafareas@users.noreply.github.com>
2023-05-11 21:51:49 +0100

    Update servidorTeste.c

--- servidorTeste.c
+++ servidorTeste.c
@@ -1,14 +1,4 @@
-#include <sys/types.h>
-#include <sys/stat.h> 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
-#include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */
-#include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
-#include <sys/wait.h> /* chamadas wait*() e macros relacionadas */
-#include <string.h>
-#include <math.h>
-
+#include "servidorTeste.h"
 
 int main(int argc, char ** argv){
 
