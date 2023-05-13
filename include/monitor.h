ece2105fc08fc04da4bf33d1c63949fa483f8312
Rafael dos Anjos Arêas <40003878+rafareas@users.noreply.github.com>
2023-05-13 18:24:13 +0100

    pasta include com o .h

---
+++ include/monitor.h
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <sys/stat.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
+#include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */
+#include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
+#include <sys/wait.h> /* chamadas wait*() e macros relacionadas */
+#include <string.h>
+#include <math.h>
+
+long int stats_time(char *command,int argc);
+void stats_uniq(char *command,int argc);
+int stats_command(char *comando1,char *comando2,int argc);
